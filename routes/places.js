7e08cef5ae610b552fccf234c8f2edb36e4aaeca
Matteo <pro.matteo.piselli@gmail.com>
2025-05-20 17:29:48 +0200

    ajout enregistrement en bdd avec l'url de l'image

--- routes/places.js
+++ routes/places.js
@@ -5,6 +5,7 @@ const authenticateToken = require("../modules/auth");
 const uniqid = require("uniqid");
 const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
+const Place = require("../models/places");
 
 router.post("/upload", authenticateToken, async (req, res) => {
   // créer un chemin d'adresse temporaite avec un id
@@ -16,7 +17,21 @@ router.post("/upload", authenticateToken, async (req, res) => {
     // s'il est vide envoi à cloudinary et delete fichier temporaire
     const resultCloudinary = await cloudinary.uploader.upload(photoPath);
     fs.unlinkSync(photoPath); // supprime le photopath
-    res.json({ result: true, url: resultCloudinary.secure_url });
+
+    // ---------- Enregistrement en BDD avec l'url de l'image ----------
+    const newPlace = new Place({
+      pictures: [resultCloudinary.secure_url],
+      signalement: 0,
+      comments: [],
+    });
+
+    const savedPlace = await newPlace.save(); // Attends puis enregistre en BDD
+
+    res.json({
+      result: true,
+      url: resultCloudinary.secure_url,
+      place: savedPlace,
+    });
   } else {
     // sinon contient le message d'erreur
     res.json({ result: false, error: resultMove });
