9973428c41e64fa6c528672d1a3b0096016f6d1c
karel <buet.karel@gmail.com>
2025-05-23 14:14:02 +0200

    mise a jour

--- routes/users.js
+++ routes/users.js
@@ -102,4 +102,76 @@ router.post("/login", (req, res) => {
     });
 });
 
+// Ajout d'un favoris de l'utilisateur
+router.post("/addFavorites", authenticateToken, (req, res) => {
+  const userEmail = req.user.email; // l'email extrait du token
+  const favoriteItem = req.body.favorite; // l'élément à ajouter aux favoris
+
+  if (!favoriteItem) {
+    return res.status(400).json({ result: false, error: "Missing favorite" });
+  }
+  User.findOne({ email: userEmail })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({
+          result: false,
+          error: "User not found",
+        });
+      }
+      // Vérification si l'élément est déjà dans les favoris
+      if (user.favorites.includes(favoriteItem)) {
+        return res.json({
+          result: false,
+          error: "Already in favorites",
+        });
+      }
+      // Ajout de l'élément aux favoris
+      user.favorites.push(favoriteItem);
+
+      user
+        .save()
+        .then((updatedUser) => {
+          res.json({
+            result: true,
+            favorites: updatedUser.favorites,
+          });
+        })
+        .catch((err) => {
+          res.status(500).json({
+            result: false,
+            error: "Database save error",
+            details: err.message,
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        result: false,
+        error: "Database find error",
+        details: err.message,
+      });
+    });
+});
+
+// Récupération des favoris de l'utilisateur
+router.get("/favorites", authenticateToken, async (req, res) => {
+  const userEmail = req.user.email; // l'email extrait du token
+
+  try {
+    // Récupération de l'utilisateur à partir de l'email
+    const user = await User.findOne({ email: userEmail });
+    if (!user) {
+      return res.json({ result: false, error: "Invalid user" });
+    }
+
+    res.json({
+      result: true,
+      favorites: user.favorites,
+    });
+  } catch (err) {
+    console.error("Favorites retrieval error:", err);
+    res.status(500).json({ result: false, error: "Internal server error" });
+  }
+});
+
 module.exports = router;
